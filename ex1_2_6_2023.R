2562ebf9572117be76f6bee0df83518fbf1d518c
dmaheeka <dmaheeka@iastate.edu>
2023-02-06 13:24:04 -0600

    update

--- ex1_2_6_2023.R
+++ ex1_2_6_2023.R
@@ -1,4 +1,4 @@
-#write a function to find square root of sum of squared elements in a vector
+#Q1: write a function to find square root of sum of squared elements in a vector
 
 #assuming n is the number of elements in a function x
 # for example n = 3 and x = c(1,2,2)..the output is 3
@@ -11,3 +11,15 @@ euclideanNorm = function(x) {
 }
 
 euclideanNorm(c(1,2,2))
+
+#Q2: write a function using a loop to find square root of sum of squared elements in a vector
+
+x = c(3:16)
+sum = 0
+
+for(i in 1: length(x)) {
+  sum = sum + x[i]^2
+}
+
+ans = sqrt(sum)
+print(ans)
